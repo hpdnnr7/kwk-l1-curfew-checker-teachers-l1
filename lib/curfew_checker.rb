bedc474ea96af062a8f24176029ae86c15b49f0e
Hope Dunner <hpdnnr7@gmail.com>
2018-08-07 15:47:27 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -13,26 +13,6 @@ def curfew_checker(time)
   end
 end
 
-def complex_curfew(time)
-  if time > 11
-    "You're in trouble! Better get home quick"
-  elsif time == 11
-    "Time to apparate!"
-  else
-    "Keep having fun!"
-  end
-end
 
-def deluxe_curfew_checker(time)
-  def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here	  if current_time == curfew_time
-    "Time to apparate!"
-  elsif current_time > curfew_time
-    "You're in trouble! Better get back to Hogwarts quick!"
-  else
-      hours = curfew_time - current_time
-      "You have #{hours} hour(s) left to keep having fun!"
-  end
-end
 
   
